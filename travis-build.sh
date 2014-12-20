cc22559f70268846f1144a45de85a0ff2b333b66
timedev <timecraftor@gmail.com>
2014-12-20 01:33:13 +0100

    Add identification to travis build

--- travis-build.sh
+++ travis-build.sh
@@ -4,5 +4,7 @@ cd temp
 git checkout gh-pages
 cp -r ../build ./
 git add -A
+git config user.name "Travis Build"
+git config user.email "oshomburg@gmail.com"
 git commit -m "push to github pages (auto)" -m "$TRAVIS_COMMIT_MSG"
 git push origin gh-pages
